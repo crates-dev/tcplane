bcc174e1b030e3b1242d28cd5140892c4e15f543
尤雨东 <root@ltpp.vip>
2025-01-15 22:01:14 +0800

    feat:v2.0.4

--- src/cfg.rs
+++ src/cfg.rs
@@ -31,6 +31,69 @@ async fn test_server_basic_usage() {
         write_data.clone()
     }
 
+    fn sync_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
+        let mut controller_data: RwLockWriteControllerData =
+            arc_lock_controller_data.write().unwrap();
+        {
+            let request: &mut Vec<u8> = controller_data.get_mut_request();
+            let mut new_request: Vec<u8> = request.clone();
+            let ext: Vec<u8> = "test".as_bytes().to_vec();
+            new_request.extend(ext);
+            *request = new_request;
+        }
+        let request: Request = controller_data.get_request().clone();
+        let stream: ArcTcpStream = controller_data.get_stream().clone().unwrap();
+        let host: String = stream
+            .peer_addr()
+            .and_then(|host| Ok(host.to_string()))
+            .unwrap_or("Unknown".to_owned());
+
+        controller_data.get_log().log_debug(
+            format!(
+                "Request host => {}\n{:#?}\n",
+                host,
+                String::from_utf8_lossy(&request),
+            ),
+            common_log,
+        );
+    }
+
+    async fn async_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
+        let controller_data: RwLockWriteControllerData = arc_lock_controller_data.write().unwrap();
+        println!(
+            "async middleware request{:?}",
+            String::from_utf8_lossy(controller_data.get_request())
+        );
+    }
+
+    fn sync_func(arc_lock_controller_data: ArcRwLockControllerData) {
+        let controller_data: RwLockWriteControllerData = arc_lock_controller_data.write().unwrap();
+        let stream: ArcTcpStream = controller_data.get_stream().clone().unwrap();
+        let res: ResponseResult = controller_data
+            .get_response()
+            .clone()
+            .set_data("hello world".into())
+            .send(&stream);
+        controller_data.get_log().log_debug(
+            format!("Response => {:?}\n", String::from_utf8_lossy(&res.unwrap())),
+            common_log,
+        );
+    }
+
+    async fn async_func(arc_lock_controller_data: ArcRwLockControllerData) {
+        let controller_data: RwLockWriteControllerData = arc_lock_controller_data.write().unwrap();
+        let stream: ArcTcpStream = controller_data.get_stream().clone().unwrap();
+        let res: ResponseResult = controller_data
+            .get_response()
+            .clone()
+            .set_data("Async".into())
+            .send(&stream);
+        controller_data.get_log().log_debug(
+            format!("Response => {:?}\n", String::from_utf8_lossy(&res.unwrap())),
+            common_log,
+        );
+    }
+
     async fn run_server() {
         let mut server: Server = Server::new();
         server.host("0.0.0.0");
@@ -39,58 +102,10 @@ async fn test_server_basic_usage() {
         server.log_dir("./logs");
         server.log_size(1_024_000);
         server.buffer(1_024_000);
-        server.middleware(|arc_lock_controller_data| {
-            let mut controller_data: RwLockWriteControllerData =
-                arc_lock_controller_data.write().unwrap();
-            {
-                let request: &mut Vec<u8> = controller_data.get_mut_request();
-                let mut new_request: Vec<u8> = request.clone();
-                let ext: Vec<u8> = "test".as_bytes().to_vec();
-                new_request.extend(ext);
-                *request = new_request;
-            }
-            let request: Request = controller_data.get_request().clone();
-            let stream: ArcTcpStream = controller_data.get_stream().clone().unwrap();
-            let host: String = stream
-                .peer_addr()
-                .and_then(|host| Ok(host.to_string()))
-                .unwrap_or("Unknown".to_owned());
-
-            controller_data.get_log().log_debug(
-                format!(
-                    "Request host => {}\n{:#?}\n",
-                    host,
-                    String::from_utf8_lossy(&request),
-                ),
-                common_log,
-            );
-        });
-
-        server
-            .async_middleware(|arc_lock_controller_data| async move {
-                let controller_data: RwLockWriteControllerData =
-                    arc_lock_controller_data.write().unwrap();
-                println!(
-                    "async middleware request{:?}",
-                    String::from_utf8_lossy(controller_data.get_request())
-                );
-            })
-            .await;
-
-        server.func(|arc_lock_controller_data| {
-            let controller_data: RwLockWriteControllerData =
-                arc_lock_controller_data.write().unwrap();
-            let stream: ArcTcpStream = controller_data.get_stream().clone().unwrap();
-            let res: ResponseResult = controller_data
-                .get_response()
-                .clone()
-                .set_data("hello world".into())
-                .send(&stream);
-            controller_data.get_log().log_debug(
-                format!("Response => {:?}\n", String::from_utf8_lossy(&res.unwrap())),
-                common_log,
-            );
-        });
+        server.middleware(sync_middleware);
+        server.async_middleware(async_middleware).await;
+        server.func(sync_func);
+        server.async_func(async_func).await;
         server.listen();
     }
 
