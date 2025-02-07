ba06bca24a8f4bea90ed2757401b761d9c907548
尤雨东 <root@ltpp.vip>
2025-02-07 23:10:26 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/tcplane.git;
+git init;
+git config core.ignorecase false;
+git remote add gitee git@gitee.com:ltpp-universe/tcplane.git;
 git remote add origin git@github.com:ltpp-universe/tcplane.git;
