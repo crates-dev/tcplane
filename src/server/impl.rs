05b83a8d3ee788fd78581e330851cc1a5b2188ec
尤雨东 <root@ltpp.vip>
2025-03-25 12:17:55 +0800

    feat: v3.14.3

--- src/server/impl.rs
+++ src/server/impl.rs
@@ -191,7 +191,7 @@ impl Server {
                 .read()
                 .await
                 .get_log()
-                .error(err.to_string(), common_log);
+                .error(&err.to_string(), common_log);
             return self;
         }
         let tcp_listener: TcpListener = listener_res.unwrap();
@@ -226,13 +226,12 @@ impl Server {
         let enable_inner_print: bool = *cfg.get_inner_print();
         let enable_inner_log: bool = *cfg.get_inner_log() && tmp.get_log().is_enable();
         set_hook(Box::new(move |err| {
-            for line in err.to_string().lines() {
-                if enable_inner_print {
-                    println_error!(line);
-                }
-                if enable_inner_log {
-                    handle_error(&tmp, line.to_string());
-                }
+            let err_string: String = err.to_string();
+            if enable_inner_print {
+                println_error!(err_string);
+            }
+            if enable_inner_log {
+                handle_error(&tmp, &err_string);
             }
         }));
     }
