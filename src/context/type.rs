18ac3d6a77dd7dc35cd3779a4454677fadf5d8a8
尤雨东 <root@ltpp.vip>
2025-12-28 11:33:27 +0800

    feat: v5.0.7

--- src/context/type.rs
+++ src/context/type.rs
@@ -2,9 +2,12 @@ use crate::*;
 
 /// A write guard for the `InnerContext`.
 pub type RwLockWriteContext<'a> = RwLockWriteGuard<'a, InnerContext>;
+
 /// A read guard for the `InnerContext`.
 pub type RwLockReadContext<'a> = RwLockReadGuard<'a, InnerContext>;
+
 /// An `Arc` pointer to a type that is `Any`, `Send`, and `Sync`.
 pub type ArcAnySendSync = Arc<dyn Any + Send + Sync>;
+
 /// A hash map storing `ArcAnySendSync` values, keyed by `String`.
 pub type HashMapArcAnySendSync = HashMap<String, ArcAnySendSync>;
